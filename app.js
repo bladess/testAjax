005cbffdeae9d05f7c73e7430aa78790391df20e
bladess <bladess@hotmail.fr>
2017-09-05 13:42:34 +0200

    hahah marche pas

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 $("#sub").on("click",function(){
-   
+    
    $.ajax({ 
-        url : 'http://192.168.1.12:8000/public/secret.json',
-        type : 'GET',
+        url : 'http://192.168.1.12:8000/users',
+        type : 'POST',
+        data : {name:'cheurfa', content:"pas d'idée"},
         success : function(data, statut){
-            $("#trois").html("le mdp est : "+data.secret);
+            console.log(data);
+            $.ajax({
+                url : 'http://192.168.1.12:8000/users',
+                type : 'GET',
+                data : "name=cheurfa",
+                succes : function(data, statut){
+                    console.log(1);
+                },
+                error : function(){
+                    console.log(3);
+                }
+            });
 
         },
             
             
         error : function(resultat, statut, erreur){
-
             
-            $("#trois").html("<h1>error</h1>");
+            $("#trois").text("<h1>error get</h1>");
         },
 
-
-        complete : function(resultat, statut){
-    
-
-        }
     });
 
 
 });
 
-        
